0591fff72e0c849699f1261788ec7787173b8298
mplotnyko <mykhaylo.plotnykov@mail.utoronto.ca>
2024-04-25 19:48:11 -0400

    setup

--- build/lib/exopie/__init__.py
+++
@@ -1,6 +0,0 @@
-
-__all__ = ["rocky", "ocean", "envelope", "get_radius"]
-
-from exopie.property import exoplanet, load_Data
-from exopie.exoplanet import rocky, ocean, envelope, get_radius
-from exopie.tools import sigma_cmf, delta_cmf, chemistry, magnisium_number
